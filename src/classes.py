2c5028cead2f3f6388ddba6f75eb62af28a530b6
Rustam Abuzyarov <rustamabuzarov56@gmail.com>
2025-11-27 19:27:13 +0500

    add method add_product

--- src/classes.py
+++ src/classes.py
@@ -27,6 +27,9 @@ class Category:
         """Инициализирует объект категории"""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
-        Category.product_count += len(products)
+
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.product_count += 1
