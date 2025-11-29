d265a52a826229d9f1e958dbbb84b284b5f2143b
Rustam Abuzyarov <rustamabuzarov56@gmail.com>
2025-11-29 23:23:31 +0500

    add tests

--- src/classes.py
+++ src/classes.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Product:
     """Представляет отдельный товар в магазине"""
 
@@ -14,26 +17,21 @@ class Product:
         self.quantity = quantity
 
     @classmethod
-    def new_product(cls, product_data: dict):
+    def new_product(cls, product_data: dict) -> Product:
         """Класс-метод, создающий экземпляр класса Product"""
-        return cls(
-            product_data ["name"],
-            product_data ["description"],
-            product_data ["price"],
-            product_data ["quantity"]
-        )
+        return cls(product_data["name"], product_data["description"], product_data["price"], product_data["quantity"])
 
     @property
-    def price(self):
+    def price(self) -> float:
         """Геттер возвращает значение приватного атрибута цены"""
         return self.__price
 
     @price.setter
-    def price(self, new_price):
-        """ Cеттер проверяет в случае если цена равна или ниже нуля, выводит сообщение в консоль
-            “Цена не должна быть нулевая или отрицательная”"""
+    def price(self, new_price: int) -> None:
+        """Cеттер в случае если цена равна или ниже нуля, выводит сообщение в консоль
+        “Цена не должна быть нулевая или отрицательная”"""
         if new_price > 0:
-            self.__price = new_price
+            self.__price = float(new_price)
         else:
             print("Цена не должна быть нулевая или отрицательная")
 
@@ -54,15 +52,54 @@ class Category:
         self.__products = products
         Category.category_count += 1
 
-    def add_product(self, product: list):
+    def add_product(self, product: Product) -> None:
         """Метод добавления товара в категорию"""
         self.__products.append(product)
         Category.product_count += 1
 
     @property
-    def products(self):
+    def products(self) -> str:
         """Геттер возвращающий список товаров"""
+        result = ""
         for product in self.__products:
-            return f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+            result += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return result
 
 
+if __name__ == "__main__":
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
+
+    print(category1.products)
+    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    category1.add_product(product4)
+    print(category1.products)
+    print(category1.product_count)
+
+    new_product = Product.new_product(
+        {
+            "name": "Samsung Galaxy S23 Ultra",
+            "description": "256GB, Серый цвет, 200MP камера",
+            "price": 180000.0,
+            "quantity": 5,
+        }
+    )
+    print(new_product.name)
+    print(new_product.description)
+    print(new_product.price)
+    print(new_product.quantity)
+
+    new_product.price = 800
+    print(new_product.price)
+
+    new_product.price = -100
+    print(new_product.price)
+    new_product.price = 0
+    print(new_product.price)
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,8 +1,42 @@
+
+from src.classes import Category, Product
+
+
 def test_product(test_data_1):
     assert test_data_1.name == "Samsung Galaxy S23 Ultra"
     assert test_data_1.description == "256GB, Серый цвет, 200MP камера"
     assert test_data_1.price == 180000.0
     assert test_data_1.quantity == 5
+
+
+def test_new_product(test_data_1):
+    assert test_data_1.name == "Samsung Galaxy S23 Ultra"
+    assert test_data_1.description == "256GB, Серый цвет, 200MP камера"
+    assert test_data_1.price == 180000.0
+    assert test_data_1.quantity == 5
+
+
+def test_price(test_data_1):
+    price_from_fixtures = test_data_1.price
+    assert price_from_fixtures == 180000.0
+
+
+def test_price_setter_valid_2(test_data_1):
+    test_data_1.price = 800
+    assert test_data_1.price == 800
+
+
+def test_price_setter_invalid_no_exception(test_data_1):
+    initial_price = test_data_1.price
+    test_data_1.price = -100
+    test_data_1.price = 0
+    assert test_data_1.price == initial_price
+
+
+def test_price_setter_invalid_prints_message(capsys, test_data_1):
+    test_data_1.price = -100
+    captured = capsys.readouterr()
+    assert "Цена не должна быть нулевая или отрицательная" in captured.out
 
 
 def test_category(test_data_2):
@@ -12,4 +46,18 @@ def test_category(test_data_2):
         == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
     )
     assert test_data_2.category_count == 1
-    assert test_data_2.product_count == 3
+
+
+def test_products_property(test_data_2):
+    expected_result = (
+        "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
+        "Iphone 15, 210000.0 руб. Остаток: 8 шт.\n"
+        "Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.\n"
+    )
+    assert test_data_2.products == expected_result, "Геттер возвращает неверный результат"
+
+
+def test_additional_product(test_data_2):
+    additional_product = Product("Google Pixel 7 Pro", "256GB, Черный", 90000.0, 3)
+    test_data_2.add_product(additional_product)
+    assert Category.product_count == 1
