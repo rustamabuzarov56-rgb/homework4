e28d5158a79fef6c520d0c0df5f8099afde0a89e
Rustam Abuzyarov <rustamabuzarov56@gmail.com>
2025-12-11 19:17:08 +0500

    refinement of methods __add__, add_product

--- src/classes.py
+++ src/classes.py
@@ -22,9 +22,11 @@ class Product:
 
     def __add__(self, other: Product) -> float:
         """Магический метод возвращает сумму произведений цены на количество."""
-        result = self.__price * self.quantity
-        result2 = other.__price * other.quantity
-        return result + result2
+        if type(self) is type(other):
+            result = self.__price * self.quantity
+            result2 = other.__price * other.quantity
+            return result + result2
+        raise TypeError("Нельзя складывать товары разных типов")
 
     @classmethod
     def new_product(cls, product_data: dict) -> Product:
@@ -45,6 +47,7 @@ class Product:
         else:
             print("Цена не должна быть нулевая или отрицательная")
 
+
 class Smartphone(Product):
     """Класс смартфоны"""
     def __init__(self, name: str, description: str, price: float, quantity: int, efficiency: str, model: str,
@@ -64,12 +67,6 @@ class LawnGrass(Product):
         self.country = country
         self.germination_period = germination_period
         self.color = color
-
-
-
-
-
-
 
 
 class Category:
@@ -98,8 +95,11 @@ class Category:
 
     def add_product(self, product: Product) -> None:
         """Метод добавления товара в категорию"""
-        self.__products.append(product)
-        Category.product_count += 1
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += 1
+        else:
+            raise TypeError("Можно добавлять продукт только класса Product и его наследников")
 
     @property
     def products(self) -> str:
@@ -108,3 +108,86 @@ class Category:
         for product in self.__products:
             result += f"{str(product)}\n"
         return result
+
+
+
+if __name__ == '__main__':
+    smartphone1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,
+                         "S23 Ultra", 256, "Серый")
+    smartphone2 = Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space")
+    smartphone3 = Smartphone("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, 90.3, "Note 11", 1024, "Синий")
+    smartphone4 = Smartphone("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, 90.3, "Note 11", 1024, "Синий")
+    # print(smartphone1.name)
+    # print(smartphone1.description)
+    # print(smartphone1.price)
+    # print(smartphone1.quantity)
+    # print(smartphone1.efficiency)
+    # print(smartphone1.model)
+    # print(smartphone1.memory)
+    # print(smartphone1.color)
+    #
+    # print(smartphone2.name)
+    # print(smartphone2.description)
+    # print(smartphone2.price)
+    # print(smartphone2.quantity)
+    # print(smartphone2.efficiency)
+    # print(smartphone2.model)
+    # print(smartphone2.memory)
+    # print(smartphone2.color)
+    #
+    # print(smartphone3.name)
+    # print(smartphone3.description)
+    # print(smartphone3.price)
+    # print(smartphone3.quantity)
+    # print(smartphone3.efficiency)
+    # print(smartphone3.model)
+    # print(smartphone3.memory)
+    # print(smartphone3.color)
+
+    grass1 = LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
+    grass2 = LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
+
+    # print(grass1.name)
+    # print(grass1.description)
+    # print(grass1.price)
+    # print(grass1.quantity)
+    # print(grass1.country)
+    # print(grass1.germination_period)
+    # print(grass1.color)
+    #
+    # print(grass2.name)
+    # print(grass2.description)
+    # print(grass2.price)
+    # print(grass2.quantity)
+    # print(grass2.country)
+    # print(grass2.germination_period)
+    # print(grass2.color)
+
+    # smartphone_sum = smartphone1 + smartphone2
+    # print(smartphone_sum)
+    #
+    # grass_sum = grass1 + grass2
+    # print(grass_sum)
+    #
+    # try:
+    #     invalid_sum = smartphone1 + grass1
+    # except TypeError:
+    #     print("Возникла ошибка TypeError при попытке сложения")
+    # else:
+    #     print("Не возникла ошибка TypeError при попытке сложения")
+
+    category_smartphones = Category("Смартфоны", "Высокотехнологичные смартфоны", [smartphone1, smartphone2])
+    category_grass = Category("Газонная трава", "Различные виды газонной травы", [grass1, grass2])
+
+    # category_smartphones.add_product(smartphone3)
+    # category_smartphones.add_product(smartphone4)
+    # print(category_smartphones.products)
+    #
+    # print(Category.product_count)
+
+    try:
+        category_smartphones.add_product(5)
+    except TypeError:
+        print("Возникла ошибка TypeError при добавлении не продукта")
+    else:
+        print("Не возникла ошибка TypeError при добавлении не продукта")
