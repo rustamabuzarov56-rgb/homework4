08adf948fa3e006932ebdf2a647715497366438c
Rustam Abuzyarov <rustamabuzarov56@gmail.com>
2025-12-24 19:56:34 +0500

    add tests

--- src/classes.py
+++ src/classes.py
@@ -160,28 +160,6 @@ class Category:
     def middle_price(self):
         """Метод который подсчитывает средний ценник всех товаров"""
         try:
-            return sum(product.price for product in self.__products) / len(self.__products)
+            return round(sum(product.price for product in self.__products) / len(self.__products), 2)
         except ZeroDivisionError:
             return 0
-
-
-
-if __name__ == '__main__':
-    try:
-        product_invalid = Product("Бракованный товар", "Неверное количество", 1000.0, 0)
-    except ValueError as e:
-        print(
-            "Возникла ошибка ValueError прерывающая работу программы при попытке добавить продукт с нулевым количеством")
-    else:
-        print("Не возникла ошибка ValueError при попытке добавить продукт с нулевым количеством")
-
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-
-    category1 = Category("Смартфоны", "Категория смартфонов", [product1, product2, product3])
-
-    print(category1.middle_price())
-
-    category_empty = Category("Пустая категория", "Категория без продуктов", [])
-    print(category_empty.middle_price())
